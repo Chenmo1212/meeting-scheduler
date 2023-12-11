8f8ebc5074f62f8839e5251a11ebfec51516ee15
chenmo <chenmo1212@qq.com>
2023-12-11 17:17:13 +0000

    feat: Create Project Structure

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Home.css';
+const Home = () => {
+    return (
+        <div className="home" aria-label="Meeting Room Booking Page">
+            Meeting Room Booking
+        </div>
+    );
+};
+
+export default Home;
