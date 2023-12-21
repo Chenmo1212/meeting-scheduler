dbac545f23672e7645fec8400d00c4bfead358fb
chenmo <chenmo1212@qq.com>
2023-12-21 17:59:46 +0000

    feat: add unit component

--- src/components/Scheduler/Scheduler.jsx
+++ src/components/Scheduler/Scheduler.jsx
@@ -1,14 +1,43 @@
 import React from 'react';
 import './Scheduler.css';
+import Unit from "./Unit";
+
+
+const onUnitMouseDown = (roomIdx, unitIdx) => {
+  console.log(`[onUnitMouseDown]: ${roomIdx}, ${unitIdx}`)
+}
+const onUnitMouseEnter = (roomIdx, unitIdx) => {
+  console.log(`[onUnitMouseEnter]: ${roomIdx}, ${unitIdx}`)
+}
+const onUnitMouseUp = (roomIdx, unitIdx) => {
+  console.log(`[onUnitMouseUp]: ${roomIdx}, ${unitIdx}`)
+}
+const onUnitDragEnter = (roomIdx, unitIdx) => {
+  console.log(`[onUnitDragEnter]: ${roomIdx}, ${unitIdx}`)
+}
+
 
 const Scheduler = () => {
-  const rooms = [1, 2, 3]
+  const rooms = [1, 2, 3];
+  const units = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
   return (
     <div className="schedule">
-      {rooms.map((room, index) => (
-        <div className="room">
-          <div className="left">{`Room ${index + 1}`}</div>
-          <div className="main">111</div>
+      {rooms.map((room, roomIdx) => (
+        <div className="room" key={roomIdx}>
+          <div className="left">{`Room ${roomIdx + 1}`}</div>
+          <div className="main">
+            {units.map((unit, unitIdx) => (
+              <Unit
+                key={unitIdx}
+                width={100 / units.length + '%'}
+                onUnitMouseDown={() => onUnitMouseDown(roomIdx, unitIdx)}
+                onUnitMouseEnter={() => onUnitMouseEnter(roomIdx, unitIdx)}
+                onUnitMouseUp={() => onUnitMouseUp(roomIdx, unitIdx)}
+                onUnitDragEnter={() => onUnitDragEnter(roomIdx, unitIdx)}
+              />
+            ))}
+          </div>
         </div>
       ))}
     </div>
---
+++ src/components/Scheduler/Unit.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Unit = ({
+                width,
+                onUnitMouseDown,
+                onUnitMouseEnter,
+                onUnitMouseUp,
+                onUnitDragEnter
+              }) => {
+  return (
+    <div
+      className="unit"
+      style={{width: width}}
+      onMouseDown={onUnitMouseDown}
+      onMouseEnter={onUnitMouseEnter}
+      onMouseUp={onUnitMouseUp}
+      onDragEnter={onUnitDragEnter}
+    />
+  );
+};
+
+export default Unit;
