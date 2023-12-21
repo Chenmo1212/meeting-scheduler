ca234b6634cebf98e8eb8c4a952fbe71ce79ea7d
chenmo <chenmo1212@qq.com>
2023-12-21 15:45:14 +0000

    style: modify the style of timeline component

--- src/components/Timeline/Timeline.jsx
+++ src/components/Timeline/Timeline.jsx
@@ -1,30 +1,61 @@
 import React, {useState} from 'react';
-import './Timeline.css'; // 导入样式文件
+import './Timeline.css';
+import {DragDropContext, Draggable, Droppable} from 'react-beautiful-dnd';
 
 const Timeline = () => {
-    // State to track the selected time slot
-    const [selectedSlot, setSelectedSlot] = useState(null);
+    const [selectedSlots, setSelectedSlots] = useState([]);
 
-    // Function to handle slot selection
     const handleSlotClick = (slot) => {
-        setSelectedSlot(slot);
+        if (selectedSlots.includes(slot)) {
+            const newSelectedSlots = selectedSlots.filter((selectedSlot) => selectedSlot !== slot);
+            setSelectedSlots(newSelectedSlots);
+        } else {
+            setSelectedSlots([...selectedSlots, slot]);
+        }
     };
 
-    // Render time slots for a 12-hour period
+    const handleDragEnd = (result) => {
+        if (!result.destination) return;
+
+
+        const startIndex = result.source.index;
+        const endIndex = result.destination.index;
+
+        const newSelectedSlots = [...selectedSlots];
+        const [removed] = newSelectedSlots.splice(startIndex, 1);
+        newSelectedSlots.splice(endIndex, 0, removed);
+
+        setSelectedSlots(newSelectedSlots);
+    };
+
+    const renderDraggableSlot = (i) => (
+        <Draggable key={`slot-${i}`} draggableId={`slot-${i}`} index={i}>
+            {(provided, snapshot) => (
+                <div
+                    ref={provided.innerRef}
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                    className={`selection-highlight ${snapshot.isDragging ? 'dragging' : ''}`}
+                />
+            )}
+        </Draggable>
+    );
+
     const renderTimeSlots = () => {
         const slots = [];
 
         for (let i = 0; i < 48; i++) {
-            const label = `${i / 2}:00`;
-            const isSelected = selectedSlot === i;
+            const label = `${i / 2}:00`; // 使用Math.floor确保得到整数
+            const isSelected = selectedSlots.includes(i);
             const isFirstSlot = i % 24 === 0;
 
             slots.push(
                 <div
-                    key={i}
-                    className={`time-slot${isFirstSlot ? ' first-slot' : ''}${isSelected ? ' selected' : ''}`}
+                    key={label}
+                    className={`time-slot ${isFirstSlot ? 'first-slot' : ''}`}
                     onClick={() => handleSlotClick(i)}
                 >
+                    {isSelected && renderDraggableSlot(i)}
                     {i === 0 && <div className="hour-label">{label}</div>}
                     {i % 2 === 0 && i !== 0 && <div className="hour-label">{label}</div>}
                 </div>
@@ -34,7 +65,18 @@ const Timeline = () => {
         return slots;
     };
 
-    return <div className="timeline">{renderTimeSlots()}</div>;
+    return (
+        <DragDropContext onDragEnd={handleDragEnd}>
+            <Droppable droppableId="timeline" direction="horizontal">
+                {(provided) => (
+                    <div ref={provided.innerRef} {...provided.droppableProps} className="timeline">
+                        {renderTimeSlots()}
+                        {provided.placeholder}
+                    </div>
+                )}
+            </Droppable>
+        </DragDropContext>
+    );
 };
 
 export default Timeline;
